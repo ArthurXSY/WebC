89df1aaa0a64ea53da16213489969decaadd17ae
xiongshiyu <2565154962@qq.com>
2020-08-26 13:34:10 +0800

    Default Changelist

---
+++ src/main/java/com/xxx/controller/WebSiteController.java
@@ -0,0 +1,9 @@
+package com.xxx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/website")
+public class WebSiteController {
+}
--- src/main/java/com/xxx/entity/WebSite.java
+++ src/main/java/com/xxx/entity/WebSite.java
@@ -1,4 +1,54 @@
 package com.xxx.entity;
 
 public class WebSite {
+    @Override
+    public String toString() {
+        return "WebSite [id=" + id + ", webname=" + webname + ", address="
+                + address + ",category="+category+",developer="+developer+"]";
+    }
+    private int id;
+    private String webname;
+    private String address;
+    private String category;
+    private String developer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWebname() {
+        return webname;
+    }
+
+    public void setWebname(String webname) {
+        this.webname = webname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
 }
